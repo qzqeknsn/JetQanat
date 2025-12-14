b29fa3922e6342564d7b799222c733a0f91e9d01
qzqeknsn <abylay342@gmail.com>
2025-12-14 21:01:39 +0500

    Initial Commit

---
+++ JetQanat/JetQanatApp.swift
@@ -0,0 +1,17 @@
+//
+//  JetQanatApp.swift
+//  JetQanat
+//
+//  Created by Abylay Zholdybay on 14.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct JetQanatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
